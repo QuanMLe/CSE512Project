291998bf39298532b263a503c83b0567077bd734
QuanMLe <qmle1>
2018-11-06 19:23:14 -0700

    Updated spark.udf.register

--- src/main/scala/cse512/SpatialQuery.scala
+++ src/main/scala/cse512/SpatialQuery.scala
@@ -32,6 +32,7 @@ object SpatialQuery extends App{
                 false
             }
         }
+
         spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>(ST_Contains))
 
         val resultDf = spark.sql("select * from point where ST_Contains('"+arg2+"',point._c0)")
@@ -73,7 +74,7 @@ object SpatialQuery extends App{
             }
         }
 
-        spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>((ST_Contains)))
+        spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>(ST_Contains))
 
         val resultDf = spark.sql("select * from rectangle,point where ST_Contains(rectangle._c0,point._c0)")
         resultDf.show()
@@ -107,7 +108,7 @@ object SpatialQuery extends App{
             }
         }
 
-        spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>((ST_Within)))
+        spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>(ST_Within))
 
         val resultDf = spark.sql("select * from point where ST_Within(point._c0,'"+arg2+"',"+arg3+")")
         resultDf.show()
@@ -143,7 +144,7 @@ object SpatialQuery extends App{
                 false
             }
         }
-        spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>((ST_Within)))
+        spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>(ST_Within))
         val resultDf = spark.sql("select * from point1 p1, point2 p2 where ST_Within(p1._c0, p2._c0, "+arg3+")")
         resultDf.show()
 
